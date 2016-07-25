0828539f969806d99f16cfa435b1e194cd9875ca
michaelphilipsmith <michaelphilipsmith@yahoo.co.uk>
2016-07-25 07:56:12 +0100

    Create more-accessible-google-translate.js

---
+++ more-accessible-google-translate.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+	jQuery('div#google_translate_element div.goog-te-gadget div.goog-te-gadget-simple > img.goog-te-gadget-icon').attr('alt', 'Google Icon');
+	jQuery('div#google_translate_element div.goog-te-gadget div.goog-te-gadget-simple span a.goog-te-menu-value > img').attr('alt', 'Invisible image used as a spacer');
+};
